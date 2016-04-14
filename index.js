bef13717dc888ce004001f1aa85f95ea49171571
rajayogan <rajayogan27@gmail.com>
2016-04-15 02:38:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function myname() {
+    
+}
+
+myname.prototype.sayname = function() {
+    return 'Raja Yogan';
+}
+
+module.exports = myname;
---
+++ test.js
@@ -0,0 +1,11 @@
+var Name = require('./index.js');
+var expect = require('chai').expect;
+
+
+describe('Firsttest', function(){
+    it('It should return my name', function(){
+        var name = new Name();
+        var result = name.sayname();
+        expect(result).to.equal('Raja Yogan');
+    })
+})
